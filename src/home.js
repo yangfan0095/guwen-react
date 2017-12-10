6e3b7fcc5500cb220b7933712e5aa922f8103cdd
范洋 <yang.fan@qiyuesuo.com>
2017-12-10 16:42:22 +0800

    更新api

--- src/home.js
+++ src/home.js
@@ -1,31 +1,63 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
-// import AMUIReact  from 'amazeui-react';
 // import counter from './reducers';
 import { connect } from 'react-redux';
-import { add, decrement} from './actions';
+import axios from 'axios';
+import { add, decrement } from './actions';
+import api from './services/api';
 import logo from './logo.svg';
 import './home.less';
-const Home = (props) =>{
+
+class Home extends Component {
+
+  addCount = () => {
+    this.props.add();
+  }
+  componentDidMount() {
+    axios.get(api.url.booklist).then((res) => {
+      
+    }).catch((err) => {
+      console.log(err.status);
+    })
+  }
+  render() {
+    const props = this.props;
+    return (
+      <div className="home">
+        <div className="title">create a react app</div>
+        <div>{props.count}</div>
+        <div onClick={this.addCount.bind(this)}>add</div>
+        <div onClick={props.decrement}>decrement</div>
+      </div>
+    );
+  }
+
+}
+/*const Home = (props) =>{
    let  addCount =()=>{
         props.add();
     };
      return (
       <div className="home">
         <div className="title">create a react app</div>
+        {console.log(props)}
         <div>{props.count}</div>
         <div onClick={addCount}>add</div>
         <div onClick={props.decrement}>decrement</div>
       </div>
     );
-}
+}*/
+
+
+
+
 
 const mapStateToProps = (state) => ({
   count: state.count,
 });
-const mapDispatchToProps ={
-    add:add,
-    decrement:decrement
+const mapDispatchToProps = {
+  add: add,
+  decrement: decrement
 }
 
 
---
+++ src/services/api.js
@@ -0,0 +1,11 @@
+ /**
+  * 配置全局api
+  */
+ const base = 'http://localhost:8080';
+ export default {
+     url: {
+         book: `${base}/book`,
+         booklist: `${base}/booklist`
+     }
+
+ }
