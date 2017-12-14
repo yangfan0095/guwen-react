a6664ad43182a8c6cbcf4e583d4249fd259f5a99
范洋 <yang.fan@qiyuesuo.com>
2017-12-14 12:19:44 +0800

    修改注释

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@
   * 配置全局api
   */
  const site = '47.52.115.169';
-//  const base = `http://${site}:8080`;
-  const base = 'http://localhost:8080';
+ const base = `http://${site}:8080`;
+//   const base = 'http://localhost:8080';
  export default {
      url: {
          book: `${base}/book`,
